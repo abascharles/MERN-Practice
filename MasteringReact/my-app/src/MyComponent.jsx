38e121f3efbb6be9b655dab1ce485ae23271a569
abascharles <abascharlesbenard@gmail.com>
2025-07-03 09:59:03 +0300

    use effect return function

--- MasteringReact/my-app/src/MyComponent.jsx
+++ MasteringReact/my-app/src/MyComponent.jsx
@@ -3,19 +3,28 @@ import { useState, UseEffect } from 'react';
 
 function MyComponent() {
   const [count, setCount] = useState(0);
+  const [color, setColor] = useState('green');
   //Use effect
   useEffect(() => {
-    document.title = `Count: ${count}`;
-  });
+    document.title = `Count: ${count} ${color}`;
+
+    //clean up code
+    return () => {};
+  }, [count]);
 
   //Function to update count
   function addCount() {
     setCount(count + 1);
   }
+
+  function changeColor() {
+    setColor(c => (c === 'green' ? 'red' : 'green'));
+  }
   return (
     <div>
-      <p>Count: {count}</p>
+      <p style={{ color: color }}>Count: {count}</p>
       <button onClick={addCount}>Add</button>
+      <button onClick={changeColor}>Change Color</button>
     </div>
   );
 }
